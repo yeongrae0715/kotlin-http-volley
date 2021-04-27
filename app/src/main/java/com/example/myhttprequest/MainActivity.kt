c5c317828461ad52ea59aeb222eb0bdb430b60fd
yeongrae <anguk0715@gmail.com>
2021-04-27 14:50:31 +0900

    - Add dependency - Add internet permission - send a simple request when button clicked

--- app/src/main/java/com/example/myhttprequest/MainActivity.kt
+++ app/src/main/java/com/example/myhttprequest/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.myhttprequest
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Request.Method.GET
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sendButton = findViewById<Button>(R.id.btSend).setOnClickListener { request() }
+
+
+    }
+    fun request(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://192.168.0.196:8080"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(GET, url,
+            { response -> Log.d("myResponse", "$response")},
+            { error -> Log.d("myResponse", "$error") })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+        Log.d("myQueue", queue.toString())
     }
 }
