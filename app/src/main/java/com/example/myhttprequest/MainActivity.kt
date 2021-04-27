ea2dbb40ff0afa9db907f0a8e52e42ffcfd93e53
yeongrae <anguk0715@gmail.com>
2021-04-27 17:52:32 +0900

    - Avoid 'out of memory size' exception : declare request queue just once.

--- app/src/main/java/com/example/myhttprequest/MainActivity.kt
+++ app/src/main/java/com/example/myhttprequest/MainActivity.kt
@@ -1,41 +1,97 @@
 package com.example.myhttprequest
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import com.android.volley.Request
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request.Method.GET
-import com.android.volley.Response
+import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import kotlin.math.log
 
+@Suppress("ClassName")
 class MainActivity : AppCompatActivity() {
+
+    var flagRequest:Boolean = false
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sendButton = findViewById<Button>(R.id.btSend).setOnClickListener { request() }
+        val sendButton = findViewById<Button>(R.id.btSend).setOnClickListener {
+            flagRequest = !flagRequest
+            Log.d("myResponse", "$flagRequest")
 
+        }
 
+        val myThread = requestAsync()
+        myThread.start()
     }
+
+    inner class requestAsync : Thread() {
+        override fun run() {
+            Log.d("myResponse", "thread started")
+            while (true){
+                if (flagRequest){
+                    try {
+                        request()
+                        sleep(100)
+                    }
+                    catch (e: Exception){
+                        Log.d("myError", "$e")
+                    }
+                }
+            }
+
+        }
+    }
+
+    var volleyCount:Int = 0
+    lateinit var resp:String
+    var queue:RequestQueue? = null
+
     fun request(){
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
+        if (queue == null){
+            queue = Volley.newRequestQueue(this)
+            Log.d("create", "I created a queue")
+        }
+
         val url = "http://192.168.0.196:8080"
 
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(GET, url,
-            { response -> Log.d("myResponse", "$response")},
-            { error -> Log.d("myResponse", "$error") })
+            { response ->
+                resp = response
+                Log.d("myResponse", "$resp / count : $volleyCount")
+
+            },
+            { error -> Log.d("myError", "$error / count : $volleyCount") })
+
+        stringRequest.setShouldCache(false) // no caching url...
+
+        stringRequest.retryPolicy = DefaultRetryPolicy(
+            20000,  //time to wait for it in this case 20s
+            20,  //tries in case of error
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest)
-        Log.d("myQueue", queue.toString())
+        try {
+//            if (volleyCount > 500) {
+////                queue.cancelAll(true)
+//                volleyCount = 0
+//            }
+            queue?.add(stringRequest)
+            Log.d("myQueue", "$queue // $volleyCount")
+            volleyCount++
+        }
+        catch (error: Exception){
+            Log.d("myError", "$error // $volleyCount")
+        }
     }
 }
